c14aeeff658bf40c33837ff1920863ec62514caf
Blair Shi <blair@chndippd06s1.data.cox.com>
2018-12-26 15:56:05 -0500

    5th commit -- configured tomcat, able to run local server to 404 page, add first sql db

--- src/main/java/com/ascending/blair/domain/User.java
+++ src/main/java/com/ascending/blair/domain/User.java
@@ -1,9 +1,31 @@
 package com.ascending.blair.domain;
 
+import com.sun.istack.internal.NotNull;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "users")
 public class User {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "users_id_seq")
+    @SequenceGenerator(name = "users_id_seq", sequenceName = "users_id_seq", allocationSize = 1)
+    private Long id;
+
+    @Column(name="user_name", unique = true)
     private String username;
+
+    @Column(name = "first_name")
     private String firstName;
+
+    @Column(name = "last_name")
     private String lastName;
+
+    @Column(name = "email", unique = true)
+    @NotNull
     private String email;
+
+    @Column(name = "password")
+    private String password;
 }
