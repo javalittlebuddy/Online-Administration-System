27712fd726f5e6bfa25d854ec0800fd777b1f85a
Blair Shi <blair@Blairs-MBP.fios-router.home>
2018-12-26 02:40:45 -0500

    4th commit -- add web.xml and coresponding config

---
+++ src/main/java/com/ascending/blair/config/DatabaseConfig.java
@@ -0,0 +1,8 @@
+package com.ascending.blair.config;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class DatabaseConfig {
+
+}
---
+++ src/main/java/com/ascending/blair/config/MvcConfig.java
@@ -0,0 +1,7 @@
+package com.ascending.blair.config;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class MvcConfig {
+}
